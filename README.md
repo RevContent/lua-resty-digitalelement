adc1de37525a0819f6ee7ce7aa1cf380bd651aa5
Richard Onyon <onyon@Richards-MacBook-Pro.local>
2022-03-17 06:38:43 -0700

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,82 @@
+# lua-resty-digitalelement
+
+Lua library for querying IPv4 addresses against DigitalElement 
+NetAcuity databases which have been exported to a JSON file. 
+
+## Installation
+
+```bash
+opm get revcontent/lua-resty-digitalelement
+```
+
+## Running
+
+```nginx
+# nginx.conf
+
+init_worker_by_lua_block {
+
+  de = require("digitalelement")
+  _, err = de.init({
+    geo = { file = "/usr/local/digitalelement/geo.json" },
+    vpn = { file = "/usr/local/digitalelement/vpn.json" }
+  })
+
+  if err then
+    for k, v in pairs(err) do
+      ngx.log(ngx.ERR, "Failed to load DigitalElement " .. k .. " database: " .. v)
+    end
+  end
+
+}
+```
+
+```lua
+
+local res, err = de.lookup("geo", ngx.var.remote_addr)
+if not res then
+  ngx.log(ngx.ERR, "IP Geo Lookup Failed: " .. err)
+else
+  ngx.say(res["country"])
+  ngx.say(res["region"])
+  ngx.say(res["city"])
+  -- ... additional database keys
+end
+
+local res, err = de.lookup("vpn", ngx.var.remote_addr)
+if not res then
+  ngx.log(ngx.ERR,"IP VPN Lookup Failed: " .. err)
+else
+  ngx.say(res["vpn-proxy-type"])
+  ngx.say(res["vpn-proxy-description"])
+  ngx.say(res["vpn-proxy-names"])
+  -- ... additional database keys
+end
+
+```
+
+### Exporting DigitalElement Files
+
+You'll need to have downloaded your databases to your local pc and install the `netacuity-textfile-creator.sh`
+utility. After doing that run the command below to generate the output file which can be imported by this package.
+
+```bash
+./netacuity-textfile-creator.sh --db_path=/path/to/db --db=1 --no_compress --output_format=json --output_file=./output.json
+```
+
+### Creating Custom IP Range Files
+
+If you want to utilize your own data to profile user IP addresses, create a file with
+each line being a single JSON string. The required key/value pairs are `start-ip` and `end-ip`.
+
+```json
+{"start-ip":"192.168.0.0","end-ip":"192.168.0.255","name":"My Home Network"}
+{"start-ip":"10.0.0.0","end-ip":"10.0.255.255","name":"My Work Network","contact":"user@domain"}
+{"start-ip":"74.2.11.3","end-ip":"74.2.11.3","name":"School","note":"Each range can have unique keys."}
+```
+
+### Future Updates
+
+- Faster importing of data
+- Support CSV & compressed files
+- Utilized Compressed Tries
---
+++ lib/resty/digitalelement.lua
@@ -0,0 +1,206 @@
+local bit         = require "bit"
+local cjson       = require "cjson"
+local iputils     = require "resty.iputils"
+local resty_sha1  = require "resty.sha1"
+local str         = require "resty.string"
+
+local rsp = {} -- Responses
+local lst = {} -- Lists
+local _M  = {} -- Local Application
+
+-- Check for a nested variable in a table, taken from
+-- https://stackoverflow.com/a/53135271 which is easiest
+-- way to validate that a JSON key exists.
+function lookup(t, ...) 
+
+  if not t then
+    return nil
+  end
+
+  for _, k in ipairs{...} do
+  
+    t = t[k]
+    if not t then
+      return nil
+    end
+  
+  end
+  return t
+
+end
+
+-- Sort JSON keys, verifying consistency between
+-- response payloads and preventing unnecessary
+-- data from being stored.
+function returnTableValuesSorted(tbl)
+
+  local v = ""
+  local ordered_keys = {}
+
+  for k in pairs(tbl) do
+      table.insert(ordered_keys, k)
+  end
+
+  table.sort(ordered_keys)
+  for i = 1, #ordered_keys do
+    v = v .. tbl[ordered_keys[i]]
+  end
+  return v
+
+end
+
+-- Create the trie to store the IP addresses
+-- by octet.
+function createTrie(key, n1, n2, n3)
+
+  -- Possibly utilize metatables to speed this up.
+  if not lst[key][n1] then
+    lst[key][n1] = {}
+  end
+  if not lst[key][n1][n2] then
+    lst[key][n1][n2] = {}
+  end
+  if not lst[key][n1][n2][n3] then
+    lst[key][n1][n2][n3] = {}
+  end
+
+  return
+
+end
+
+-- Load databases into memory.
+function init_file(key, db)
+
+  -- Debug Metrics
+  local ips = 0
+
+  -- Setup Keypair, that way we will always
+  -- be able to respond even if there is an
+  -- error loading the file. 
+  lst[key] = {}
+  rsp[key] = {}
+
+  -- Validate we can read the file
+  local f = io.open(db.file, "r")
+  if f == nil then
+    return nil, "Unable to open file " .. db.file .. "."
+  end
+  io.close(f)
+
+  -- Read file line by line
+  for l in io.lines(db.file) do
+
+    -- Decode JSON, ignore lines with error
+    local valid, entry = pcall(cjson.decode, l)
+
+    if valid then
+
+      -- Turn the start and end IP into decimals.
+      local s_ip, _ = iputils.ip2bin(entry["start-ip"])
+      local e_ip, _ = iputils.ip2bin(entry["end-ip"])
+
+      -- Remove unnecessary keys from response payload.
+      entry["start-ip"] = nil
+      entry["end-ip"]   = nil
+
+      -- If response payload does not exist yet, create it. We return table
+      -- values in a sorted array, then hash the value of that to get a unique
+      -- representation of an profile response.
+      local sha1 = resty_sha1:new()
+      sha1:update(returnTableValuesSorted(entry))
+      local digest = sha1:final()
+      if not rsp[key][digest] then
+        rsp[key][digest] = entry
+      end
+
+      -- Iterate over IP range
+      for i = s_ip, e_ip
+      do
+
+        -- Turn int back into IP notation
+        local n1 = bit.band(bit.rshift(i, 24), 255)
+        local n2 = bit.band(bit.rshift(i, 16), 255)
+        local n3 = bit.band(bit.rshift(i, 8), 255)
+        local n4 = bit.band(bit.rshift(i, 0), 255)
+
+        -- Create/Validate Trie
+        createTrie(key, n1, n2, n3)
+        table.insert(lst[key][n1][n2][n3], n4, digest)
+
+        -- Metrics
+        ips = ips + 1
+      
+      end
+
+    end
+
+  end
+
+  return ips, nil
+
+end
+
+-- Initialize Package
+function _M.init(i)
+
+  local errors = {}
+
+  -- Iterate over each database input, load into memory.
+  for k, v in pairs(i) do
+
+    ngx.log(ngx.INFO, "Initializing " .. k .. " database from file " .. v.file ..".")
+
+    ips, err = init_file(k, v)
+
+    if err ~= nil then
+      ngx.log(ngx.ERR, "Initialization Error: " .. err)
+    else
+      ngx.log(ngx.INFO, "Initialization Success, " .. ips .. " IPs loaded.")
+    end
+    
+  end
+
+  -- Reserving [1] return location for future updates
+  -- and to preserve backward compatability.
+  return nil, errors
+
+end
+
+-- Check to see if the IP is loaded into the trie.
+function _M.lookup(key, ip)
+
+  local err   = nil
+  local iparr = {}
+
+  -- Break down IP into each individual octet, load into
+  -- a table for comparision against trie.
+  for i in string.gmatch(ip, "%d+") do 
+    table.insert(iparr, i)
+  end
+
+  -- Do we have 4 octets
+  if table.getn(iparr) ~= 4 then
+    return nil, "Invalid IP: " .. ip
+  end
+
+  -- Convert each octet to integer, validate between
+  -- 0 and 255.
+  for i=1, 4
+  do
+    iparr[i] = tonumber(iparr[i])
+    if iparr[i] < 0 or 255 < iparr[i] then
+      return nil, "Invalid IP: " .. ip
+    end
+  end
+
+  -- Check to see if value exists in Trie
+  local profile = lookup(lst[key], iparr[1], iparr[2], iparr[3], iparr[4])
+  if profile == nil then
+    return nil, "No Result."
+  else
+    return rsp[key][profile], nil
+  end
+
+end
+
+return _M
